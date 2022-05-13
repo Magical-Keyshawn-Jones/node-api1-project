e5051382c404b417deb49d50e805de5498babf37
Keyshawn Jones <keyshawnjones29@gmail.com>
2022-05-13 18:00:17 -0500

    I set up server and put, get, and delete endpoints

--- api/server.js
+++ api/server.js
@@ -1,3 +1,77 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const user = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    // Storing Values
+    const { name, bio } = req.body
+    // const name = req.body.name
+    // const bio = req.body.bio
+
+    // Checking for name and bio
+    if (name === undefined || bio === undefined) {
+        return res.json({ message: 'Please provide name and bio for the user' })
+    } else {
+        user.insert({ name, bio })
+        .then(object => {
+            res.status(201).json(object)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'There was an error while saving the user to the database'})
+        })
+    }
+})
+
+server.get('/api/users', (req, res) => {
+    user.find()
+    .then(object => {
+        res.json(object)
+    })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    // Storing Id 
+    const { id } = req.params
+
+    user.findById(id)
+    .then(object => {
+        if (object) {
+            res.status(200).json(object)
+        } else {
+            res.status(400).json({ message: 'The user with the specified ID does not exist'})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).status.json({ message: 'The user information could not be retrieved'})
+    })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    // Storing id 
+    const { id } = req.params
+    
+    // Checking if user exist
+    user.findById(id)
+    .then(object => {
+        if (object) {
+            user.delete(id)
+            .then(object => {
+                res.status.json(object)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({ message: 'The user could not be removed'})
+            })
+        } else {
+            res.status(404).json({ message: 'The user with the specified ID does not exist'})
+        } 
+    })
+})
+
+module.exports = server // EXPORT YOUR SERVER instead of {}
